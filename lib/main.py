7cb3a80fc7e3e50c065e02ccc25c8de76bddbbc4
Clair <clair.karanja@student.moringaschool.com>
2023-12-13 02:03:59 +0300

    completed assignment

--- lib/main.py
+++ lib/main.py
@@ -1,33 +1,59 @@
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from models import Base, Restaurant, Customer, Review
-from database import Session
+
+# Database connection setup
+engine = create_engine('sqlite:///restaurant_reviews.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
 
 # Create a session
 session = Session()
+
+# Clear tables before adding data
+session.query(Restaurant).delete()
+session.query(Customer).delete()
+session.query(Review).delete()
+session.commit()
 
 # Add data
 restaurant1 = Restaurant(name='sarova hotel', price=2000)
 restaurant2 = Restaurant(name='Mishis hotel', price=5722)
 restaurant3 = Restaurant(name='mara hotel', price=430)
+session.add_all([restaurant1, restaurant2, restaurant3])
 
 customer1 = Customer(first_name='Clair', last_name='Karanja')
 customer2 = Customer(first_name='Basil', last_name='Kamande')
 customer3 = Customer(first_name='Anyango', last_name='Okeochi')
+session.add_all([customer1, customer2, customer3])
 
 review1 = Review(star_rating=4, restaurant=restaurant1, customer=customer1)
 review2 = Review(star_rating=5, restaurant=restaurant2, customer=customer2)
 review3 = Review(star_rating=5, restaurant=restaurant3, customer=customer3)
+session.add_all([review1, review2, review3])
 
-session.add_all([restaurant1, restaurant2, restaurant3, customer1, customer2, customer3, review1, review2, review3])
 session.commit()
 
-# Perform queries
-customer1_reviews = session.query(Customer).filter_by(first_name='Clair').first().reviews
-for review in customer1_reviews:
-    print(f"Review for {review.restaurant.name} ({review.restaurant.id}), Price: {review.restaurant.price}, by {review.customer.full_name()} ({review.customer.id}): {review.star_rating} stars")
+# Print information from the tables with headers
+print("\n--- Restaurants ---")
+print("ID\tName\tPrice")
+restaurants = session.query(Restaurant).all()
+for restaurant in restaurants:
+    print(f"{restaurant.id}\t{restaurant.name}\t{restaurant.price}")
 
-customer2_reviews = session.query(Customer).filter_by(first_name='Basil').first().reviews
-for review in customer2_reviews:
-    print(f"Review for {review.restaurant.name} ({review.restaurant.id}), Price: {review.restaurant.price}, by {review.customer.full_name()} ({review.customer.id}): {review.star_rating} stars")
+print("\n--- Customers ---")
+print("ID\tFirst Name\tLast Name")
+customers = session.query(Customer).all()
+for customer in customers:
+    print(f"{customer.id}\t{customer.first_name}\t{customer.last_name}")
+
+print("\n--- Reviews ---")
+print("ID\tRating\tRestaurant Name\tFirst Name\tLast Name")
+reviews = session.query(Review).all()
+for review in reviews:
+    print(f"{review.id}\t{review.star_rating}\t{review.restaurant.name}\t{review.customer.first_name}\t{review.customer.last_name}")
 
 # Close the session
 session.close()
+
--- lib/models.py
+++ lib/models.py
@@ -1,37 +1,37 @@
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class Restaurant(Base):
     __tablename__ = 'restaurants'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    price = Column(Integer)
+    id = Column(Integer, primary_key=True, comment='Unique ID for restaurant')
+    name = Column(String, comment='Name of the restaurant')
+    price = Column(Integer, comment='Price range of the restaurant')
     reviews = relationship("Review", back_populates="restaurant")
-    customers = relationship("Customer", secondary="reviews", overlaps="reviews")
 
 class Customer(Base):
     __tablename__ = 'customers'
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    reviews = relationship("Review", back_populates="customer", overlaps="customers")
+    id = Column(Integer, primary_key=True, comment='Unique ID for customer')
+    first_name = Column(String, comment='First name of the customer')
+    last_name = Column(String, comment='Last name of the customer')
+    reviews = relationship("Review", back_populates="customer")
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
 
+    def restaurants(self):
+        return [review.restaurant for review in self.reviews]
+
 class Review(Base):
     __tablename__ = 'reviews'
-    id = Column(Integer, primary_key=True)
-    star_rating = Column(Integer)
-    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
-    customer_id = Column(Integer, ForeignKey('customers.id'))
-    restaurant = relationship("Restaurant", back_populates="reviews", overlaps="customers", foreign_keys=[restaurant_id])
-    customer = relationship("Customer", back_populates="reviews", overlaps="customers", foreign_keys=[customer_id])
+    id = Column(Integer, primary_key=True, comment='Unique ID for review')
+    star_rating = Column(Integer, comment='Rating for the review')
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'), comment='ID of the associated restaurant')
+    customer_id = Column(Integer, ForeignKey('customers.id'), comment='ID of the associated customer')
+    restaurant = relationship("Restaurant", back_populates="reviews")
+    customer = relationship("Customer", back_populates="reviews")
 
     def full_review(self):
         return f"Review for {self.restaurant.name} by {self.customer.full_name()}: {self.star_rating} stars"
-
-
